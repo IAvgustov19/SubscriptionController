ce617d6f8fe3c55b1d63db7b37392113ef9ad6dc
avgus <avgus@IPC>
2023-06-24 16:59:52 +0300

    Add project files.

---
+++ TimelineLibrary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace TimelineLibrary
+{
+    public class Token
+    {
+        public string? Access_token { get; set; }
+    }
+}
